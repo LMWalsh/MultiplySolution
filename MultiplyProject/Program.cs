2b725c67cf8c035464c212fb781d1dc7439d6375
max-student <max-student@maxtrain.com>
2020-02-10 17:51:45 -0500

    Initial commit

---
+++ MultiplyProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MultiplyProject {
+    class Program {
+        static void Main(string[] args) {
+            var sum = 1;
+            for (var index = 2; index <=10; index++) {
+                sum *= index;
+                }
+            Console.WriteLine($"Sum is {sum}");
+            }
+        }
+    }
